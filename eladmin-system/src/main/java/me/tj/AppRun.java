f2533d8ffd966f49a23631e386e8667e481c3f3a
LTJ <597432232@qq.com>
2021-06-08 18:05:42 +0800

    Initial commit

---
+++ eladmin-system/src/main/java/me/tj/AppRun.java
@@ -0,0 +1,16 @@
+package me.tj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LTJ
+ * @version 1.0
+ * @date 2021/6/8 9:58
+ */
+@SpringBootApplication
+public class AppRun {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+}
